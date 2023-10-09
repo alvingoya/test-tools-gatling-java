6a924264ffcb40f927212ae3865ffc99e49c6177
Goya <agoya@test.com>
2023-10-09 21:20:45 +0800

    Initial commit

---
+++ src/test/java/game/test/suite/Globals.java
@@ -0,0 +1,21 @@
+package game.test.suite;
+
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import java.util.Map;
+
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+public final class Globals {
+    public static final String BASE_URL = "https://game99.feiwindevelopment.com";
+    public static final String PLATFORM_APP = "/game99-platform-app";
+    public static final Map<String, String> STATIC_HEADERS = Map.of(
+            "dev", "2",
+            "version", "1.0",
+            "frond-host", "http://localhost.com");
+    public static final HttpProtocolBuilder DEFAULT_PROTOCOL = http.baseUrl(BASE_URL)
+            .acceptHeader("application/json, text/html")
+            .acceptEncodingHeader("gzip, deflate")
+            .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36");
+
+}
---
+++ src/test/java/game/test/suite/simulation/UserLogin.java
@@ -0,0 +1,30 @@
+package game.test.suite.simulation;
+
+import static game.test.suite.Globals.*;
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+//should be public
+public class UserLogin extends Simulation {
+
+    ChainBuilder login = exec(
+            http("Login")
+                    .post(PLATFORM_APP + "/login")
+                    .asJson()
+                    .headers(STATIC_HEADERS)
+                    .body(RawFileBody("params/loginBody.json"))
+                    .check(status().is(200))
+    );
+
+    ScenarioBuilder loginScn = scenario("loginScn").exec(login);
+
+    {
+        setUp(
+                loginScn.injectOpen(atOnceUsers(1))
+        ).protocols(DEFAULT_PROTOCOL);
+    }
+
+}
